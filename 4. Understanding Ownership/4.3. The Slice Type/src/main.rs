df5742cdab2d74e3e3935ba9a051e420c21c24b6
donghui-0126 <lukehui@naver.com>
2024-04-19 01:37:05 +0900

    0418 (0419) 슬슬 어렵다..

--- 4. Understanding Ownership/4.3. The Slice Type/src/main.rs
+++ 4. Understanding Ownership/4.3. The Slice Type/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+
+    let hello = &s[..5];
+    {
+        let world = &s[6..11];
+        println!("{}", world);
+    }
+    // println!("{}", hello);    // println!을 해서 hello의 scope를 끝내버린다. 
+    
+    let mut_pointer = &mut s; // 여기서 가변 참조자를 만든다.
+    // println!("{}", hello); <- 이렇게 쓰면 hello의 scope가 끝났기 때문에 오류가 뜬다.
+    println!("{}", mut_pointer);
+
+    let hello = &s[..5];
+    println!("{}", hello);
+
+
+    let slice = slice(&s[..]);
+    println!("{}", slice);
+    
+    let slice = &slice[..];
+}
+
+fn slice(s:&str) -> &str{
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s
 }
---
+++ 4. Understanding Ownership/4.3. The Slice Type/target/debug/deps/error_code-00f8c8e531be6ffe.d
@@ -0,0 +1,5 @@
+/root/Hello-RUST-World/4. Understanding Ownership/4.3. The Slice Type/target/debug/deps/error_code-00f8c8e531be6ffe: error_code/src/main.rs
+
+/root/Hello-RUST-World/4. Understanding Ownership/4.3. The Slice Type/target/debug/deps/error_code-00f8c8e531be6ffe.d: error_code/src/main.rs
+
+error_code/src/main.rs:
